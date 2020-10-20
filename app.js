876636c9e4a1a80d1446367a28ad9c34f3f39d12
x3Dima <x3dima.romanenko@gmail.com>
2020-10-20 20:01:20 +0300

    credit calculator 2.0

--- app.js
+++ app.js
@@ -10,29 +10,58 @@ let monthDebt = (
 
 let mainDebt = monthDebt * term;
 let monthProcent = 0;
+let mainProcent = 0;
 let monthMainDebt = 0;
+let generalDebt = credit;
+let sumWithProc = 0;
 
 for (let i = 1; i < term + 1; i++) {
   if (i == 1) {
-    monthProcent = ((credit * (procent / 100)) / 12).toFixed(2);
+    monthProcent = ((generalDebt * (procent / 100)) / 12).toFixed(2);
     monthMainDebt = (monthDebt - monthProcent).toFixed(2);
+    mainProcent += +monthProcent;
+    sumWithProc += +monthDebt;
     document.write(
       `
         <div>
-          ${i} месяц,процент по кредиту: <span>${monthProcent}</span>,погашение кредита: <span>${monthMainDebt}</span>, общая выплата за месяц: <span>${monthDebt}</span>, задолженость по кредиту: <span>${credit.toFixed(
+          ${i} месяц,процент по кредиту: <span>${monthProcent}</span>,погашение кредита: <span>${monthMainDebt}</span>, общая выплата за месяц: <span>${monthDebt}</span>, задолженость по кредиту: <span>${generalDebt.toFixed(
         2
       )}</span> </br>
         </div>
       `
     );
-  } else if (i > 1) {
-    credit = credit - monthMainDebt;
-    monthProcent = ((credit * (procent / 100)) / 12).toFixed(2);
+  } else if (i > 1 && i < term) {
+    generalDebt = generalDebt - monthMainDebt;
+    monthProcent = ((generalDebt * (procent / 100)) / 12).toFixed(2);
     monthMainDebt = (monthDebt - monthProcent).toFixed(2);
+    mainProcent += +monthProcent;
+    sumWithProc += +monthDebt;
+
     document.write(
       `
         <div>
-          ${i} месяц,процент по кредиту: <span>${monthProcent}</span>,погашение кредита: <span>${monthMainDebt}</span>, общая выплата за месяц: <span>${monthDebt}</span>, задолженость по кредиту: <span>${credit.toFixed(
+          ${i} месяц,процент по кредиту: <span>${monthProcent}</span>,погашение кредита: <span>${monthMainDebt}</span>, общая выплата за месяц: <span>${monthDebt}</span>, задолженость по кредиту: <span>${generalDebt.toFixed(
+        2
+      )}</span> </br>
+        </div>
+      `
+    );
+  } else {
+    generalDebt = generalDebt - monthMainDebt;
+    monthProcent = ((generalDebt * (procent / 100)) / 12).toFixed(2);
+    mainProcent += +monthProcent;
+    mainProcent = Math.ceil(mainProcent * 100) / 100;
+    monthDebt = (credit + mainProcent - monthDebt * (term - 1)).toFixed(2);
+
+    monthMainDebt = (credit + mainProcent - sumWithProc - monthProcent).toFixed(
+      2
+    );
+    sumWithProc += +monthDebt;
+
+    document.write(
+      `
+        <div>
+          ${i} месяц,процент по кредиту: <span>${monthProcent}</span>,погашение кредита: <span>${monthMainDebt}</span>, общая выплата за месяц: <span>${monthDebt}</span>, задолженость по кредиту: <span>${generalDebt.toFixed(
         2
       )}</span> </br>
         </div>
@@ -40,5 +69,11 @@ for (let i = 1; i < term + 1; i++) {
     );
   }
 }
-
-
+document.write(
+  `
+    <div>
+       Общий процент по кредиту: <span>${mainProcent}</span>,
+        общая сумма с учетом процентов <span>${credit + mainProcent}</span>
+    </div>
+  `
+);
